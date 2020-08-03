4c46968da910070fab60cd95902d03e84f4d7b19
jim4067 <jimmyimpulse2@gmail.com>
2020-08-03 22:43:08 +0300

    started building the InputTodo component :tada:

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InputTodo = () => {
+
+    return (
+        <div>
+        <h1>I am freaking awesome</h1>
+        </div>
+    );
+}
+
+export default InputTodo;

0c2c609e97c9aea6ce30242a9a383a348d8f8390
jim4067 <jimmyimpulse2@gmail.com>
2020-08-03 23:15:20 +0300

    added css styles inline :yay:

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+const header_styles = {
+    "margin-top" : "40px",
+    "text-align" : "center"
+}
+const input_styles = {
+    "height" : "30px",
+    "margin-left" : "220px",
+    "margin-top" : "50px",
+    "width" : "50%"
+}
+const button_styles = {
+    "background" : "light-green",
+    "border" : "none",
+    "height" : "37px",
+    "margin-left" : "10px",
+    "text-align" : "center",
+    "width" : "70px"
+}
 
 const InputTodo = () => {
 
     return (
-        <div>
-        <h1>I am freaking awesome</h1>
-        </div>
+        <Fragment>
+            <h1 style={header_styles}>Pern Todo List</h1>
+            <form>
+                <input type='text ' style={input_styles}/>
+                <button style={button_styles}>Add</button>
+            </form>
+        </Fragment>
     );
 }
 

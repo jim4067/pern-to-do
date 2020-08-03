809af3df7c02b9b10b0551d886fe6c70bcde8c8c
jim4067 <jimmyimpulse2@gmail.com>
2020-08-03 23:45:48 +0300

    finished the inputTodo. takes in the users input using forms :rocket:

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,33 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const header_styles = {
-    "margin-top" : "40px",
-    "text-align" : "center"
+    "display": "flex",
+    "justifyContent": "center",
+    "marginTop": "60px",
+    //"textAlign" : "center"
 }
 const input_styles = {
-    "height" : "30px",
-    "margin-left" : "220px",
-    "margin-top" : "50px",
-    "width" : "50%"
+    "height": "30px",
+    "marginLeft": "240px",
+    "marginTop": "50px",
+    "width": "50%"
 }
 const button_styles = {
-    "background" : "light-green",
-    "border" : "none",
-    "height" : "37px",
-    "margin-left" : "10px",
-    "text-align" : "center",
-    "width" : "70px"
+    "background": "light-green",
+    "border": "none",
+    "height": "37px",
+    "marginLeft": "10px",
+    "textAlign": "center",
+    "width": "70px"
 }
 
 const InputTodo = () => {
 
+    const [description, setDescription] = useState("");
+
+    const handle_text_change = (event) => {
+        setDescription(event.target.value);
+        //console.log(event.target.value);
+    }
+
+    const on_submit_form = async (event) => {
+        event.preventDefault();
+        try {
+            const body = { description };
+            const response = await fetch('http://localhost:3040/todos', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body)
+            });
+
+            console.log("the response.......", response);
+        } catch (err) {
+            console.error("Ahh shit... ", err.message);
+        }
+    }
+
     return (
         <Fragment>
             <h1 style={header_styles}>Pern Todo List</h1>
-            <form>
-                <input type='text ' style={input_styles}/>
+
+            <form onSubmit={on_submit_form}>
+                <input onChange={handle_text_change} type='text ' style={input_styles} value={description} />
                 <button style={button_styles}>Add</button>
             </form>
+
         </Fragment>
     );
 }
